970e3c1d4548ccdc5732f0e5bbd252d2cd19f905
Sandile Khendle <36414340+Skhendle@users.noreply.github.com>
2019-10-08 23:00:08 +0200

    Update allocator.c
    Finished section 4.2 Allocation

--- hw2/allocator.c
+++ hw2/allocator.c
@@ -1,4 +1,5 @@
 // allocator.c
+#define _DEFAULT_SOURCE
 #include "allocator.h"
 #include <stdio.h>
 #include <unistd.h>
@@ -7,8 +8,44 @@
 
 /** Allocate a block of at least the requested size **/
 void* custom_malloc(size_t size) {
-    // TODO
-    return NULL;
+
+	if(size == 0){
+		return NULL;
+	}else{
+		head=(struct block*)sbrk(MAX_SIZE);
+		head->free=true;
+		head->size=MAX_SIZE;
+		head->data=(void *)((void *)head+sizeof(struct block));
+		struct block* t=head;
+
+		int i = 0;
+		while(true){
+			if(t->size==findSize(size+sizeof(struct block))){
+				break;
+			}
+			t->size=t->size/2;
+
+			struct block* budy=(struct block*)((void *)t+t->size);
+			budy->free=true;
+			budy->size=t->size;
+
+
+			struct block* next=(struct block*)((void *)t+t->size);
+			next->data=(void *)((void *)t+sizeof(struct block));
+			next->free=true;
+			next->size=t->size;
+			budy->buddy = next;
+			next->buddy = next->next;
+			next->next=t->next;
+			t->next=next;
+
+
+		}
+		t->free = false;
+
+		return (void *)t->data;
+	}
+
 }
 
 /** Mark a data block as free and merge free buddy blocks **/
@@ -21,7 +58,21 @@ void* custom_realloc(void* ptr, size_t size) {
     // TODO
     return NULL;
 }
+int findSize(size_t size){
 
+	size_t num = MAX_SIZE;
+	bool a = true;
+	while(a){
+		if(size > num){
+			a = false;
+			break;
+		}else{
+			num = num /2;
+		}
+	}
+
+	return 2*num;
+}
 /*------------------------------------*\
 |            DEBUG FUNCTIONS           |
 \*------------------------------------*/
@@ -38,8 +89,8 @@ void print_block(struct block* b) {
         printf("Free = %s\n",(b->free)?"true":"false");
         printf("Data = %p\n",b->data);
         printf("Next = %p\n",b->next);
-        printf("Buddy = %p\n",b->buddy);    
-        printf("Merge Buddies = "); 
+        printf("Buddy = %p\n",b->buddy);
+        printf("Merge Buddies = ");
         while(b->merge_buddy[i] && i < MAX_EXP) {
             printf("%p, ",b->merge_buddy[i]);
             i++;
